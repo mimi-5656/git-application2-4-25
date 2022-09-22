9a84ab057b6d88dd4d4d6e51a3435def49fdb7de
EC2 Default User <ec2-user@ip-172-31-0-52.ap-northeast-1.compute.internal>
2022-09-22 13:07:26 +0000

    sign in 終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
+
    protect_from_forgery with: :null_session
    before_action :authenticate_user!, except: [:top]
    before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    books_path
+    user_path(current_user)
   end
 
 
@@ -15,7 +16,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
   end
 
 end
+
--- db/migrate/20220921054150_create_books.rb
+++ db/migrate/20220921054150_create_books.rb
@@ -4,7 +4,7 @@ class CreateBooks < ActiveRecord::Migration[6.1]
 
 
       t.string :title
-      t.text :option
+      t.text :body
       t.integer :user_id
 
 
